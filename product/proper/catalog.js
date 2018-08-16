8eba78fb2fb34d10d9d51906b3a99a39d7dcf643
Travis Newgren <travisnewgren@gmail.com>
2018-08-16 00:21:10 -0700

    up

---
+++ product/proper/catalog.js
@@ -0,0 +1,33 @@
+export default
+{
+	"items": [
+    {
+        "name":"Mouse",
+        "image_url": "mouse",
+        "description": "Everyone's favorite mouse.",
+        "price": 22.95,
+        "tags": ["shirt", "clothes"]
+    },
+    {
+			"name": "Conquista Natural",
+			"image_url": "conquista_natural",
+			"description": "classic",
+			"price": 75.00,
+			"tags": ["shoes", "clothes"]
+		},
+		{
+			"name": "Feliz T-Shirt",
+			"image_url": "feliz_teal",
+			"description": "\"you messed with the wrong cat\"",
+			"price": 22.95,
+			"tags": ["shirt", "clothes"]
+		},
+		{
+			"name": "Conquista Black",
+			"image_url": "conquista_black",
+			"description": "now in black",
+			"price": 75.00,
+			"tags": ["shoes", "clothes"]
+		}
+	]
+}
