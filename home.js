bfa680f3333218ba1915228a1244847de57cb396
Travis Newgren <travisnewgren@gmail.com>
2018-09-26 04:48:38 -0500

    pre-brutal\!

--- home.js
+++ home.js
@@ -10,8 +10,24 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 
 var e = React.createElement;
 
+var entered = false;
 var x, y;
 var tailLength = 45;
+
+// enter site
+
+//TODO: swap two lines below
+document.onclick = function () {
+  //window.onload = () => {
+  entered = true;
+  var presses = document.getElementsByClassName('press');
+  for (var i = 0; i < presses.length; i++) {
+    if (presses[i].id !== 'nodelete') {
+      presses[i].style.display = 'none';
+    }
+  }
+  document.getElementsByClassName('over')[0].className = 'over entered';
+};
 
 var Home = function (_React$Component) {
   _inherits(Home, _React$Component);
@@ -28,18 +44,26 @@ var Home = function (_React$Component) {
   _createClass(Home, [{
     key: 'componentDidMount',
     value: function componentDidMount() {
+      // let isDesktop = () => {
+      //   console.log(document.getElementById('homeMobile').style);
+      //   document.getElementById('homeDesktop').style.display !== 'none';
+      // }
+
+
       var element = this.refs.elem;
       var animationIsActive = false;
       var framesPerSecond = 60;
       var intervalDelay = Math.round(1000 / framesPerSecond);
       //let lagTailLength = 5;
 
-
       var i = 0;
       var z = 0;
 
-      var startAnimation = function startAnimation() {
+      // if(!isDesktop()) {
+      //   return;
+      // }
 
+      var startAnimation = function startAnimation() {
         return window.setInterval(function () {
           console.log('go');
           i = (i + 1) % tailLength;
@@ -54,7 +78,7 @@ var Home = function (_React$Component) {
       var interval = null; // id of interval callback to animate
       window.addEventListener('mousemove', function (event) {
         window.clearTimeout(timeout);
-        if (!animationIsActive) {
+        if (!animationIsActive && !entered) {
           interval = startAnimation();
           animationIsActive = true;
         }
@@ -75,39 +99,152 @@ var Home = function (_React$Component) {
     value: function render() {
       return React.createElement(
         'div',
-        { ref: 'cont' },
+        { className: 'home' },
         React.createElement(
           'div',
-          { className: 'home center' },
+          { className: 'homeDesktop', id: 'homeDesktop' },
           React.createElement(
             'div',
-            { className: 'welcome' },
+            { className: 'home center' },
             React.createElement(
-              'svg',
-              { viewBox: '0 0 417 60' },
+              'div',
+              { className: 'welcome' },
               React.createElement(
-                'text',
-                { y: '57' },
-                'WELCOME'
+                'svg',
+                { viewBox: '0 0 417 60' },
+                React.createElement(
+                  'text',
+                  { y: '57' },
+                  'WELCOME'
+                )
+              )
+            )
+          ),
+          Array.apply(null, Array(tailLength)).map(function (i, j) {
+            return React.createElement(
+              'div',
+              { id: 'n' + j, key: j, className: 'press', ref: j === 0 ? 'elem' : '' },
+              React.createElement(
+                'svg',
+                { viewBox: '0 0 417 60' },
+                React.createElement(
+                  'text',
+                  { y: '57' },
+                  'PRESS'
+                )
+              )
+            );
+          }),
+          React.createElement(
+            'div',
+            { className: 'over' },
+            React.createElement(
+              'div',
+              { className: 'left' },
+              React.createElement(
+                'div',
+                { className: 'welcome rot' },
+                React.createElement(
+                  'svg',
+                  { viewBox: '0 0 417 60' },
+                  React.createElement(
+                    'text',
+                    { y: '57' },
+                    'WELCOME'
+                  )
+                )
+              ),
+              React.createElement(
+                'div',
+                { className: 'press rot', id: 'nodelete' },
+                React.createElement(
+                  'svg',
+                  { viewBox: '0 0 417 60' },
+                  React.createElement(
+                    'text',
+                    { y: '57' },
+                    'PRESS'
+                  )
+                )
+              )
+            ),
+            React.createElement(
+              'div',
+              { className: 'right' },
+              React.createElement(
+                'div',
+                { className: 'top' },
+                React.createElement(
+                  'svg',
+                  { viewBox: '0 0 417 60' },
+                  React.createElement(
+                    'text',
+                    { x: '50%', y: '50%', alignmentBaseline: 'middle', textAnchor: 'middle' },
+                    'WORK'
+                  )
+                )
+              ),
+              React.createElement(
+                'div',
+                { className: 'bottom' },
+                React.createElement(
+                  'svg',
+                  { viewBox: '0 0 417 60' },
+                  React.createElement(
+                    'text',
+                    { x: '50%', y: '50%', alignmentBaseline: 'middle', textAnchor: 'middle' },
+                    'SHOP'
+                  )
+                )
               )
             )
           )
         ),
-        Array.apply(null, Array(tailLength)).map(function (i, j) {
-          return React.createElement(
+        React.createElement(
+          'div',
+          { className: 'homeMobile', id: 'homeMobile', ref: 'homeMobile' },
+          React.createElement(
             'div',
-            { id: 'n' + j, key: j, className: 'press', ref: j === 0 ? 'elem' : '' },
+            { className: 'homeBox' },
             React.createElement(
-              'svg',
-              { viewBox: '0 0 417 60' },
+              'div',
+              { className: 'welcome' },
               React.createElement(
-                'text',
-                { y: '57' },
-                'PRESS'
+                'svg',
+                { viewBox: '0 0 417 60' },
+                React.createElement(
+                  'text',
+                  { y: '57' },
+                  'WELCOME'
+                )
+              )
+            ),
+            React.createElement(
+              'div',
+              { className: 'press' },
+              React.createElement(
+                'svg',
+                { viewBox: '0 0 417 60' },
+                React.createElement(
+                  'text',
+                  { y: '57' },
+                  'PRESS'
+                )
+              )
+            ),
+            React.createElement(
+              'div',
+              { className: 'questionBox' },
+              React.createElement(
+                'span',
+                null,
+                '?'
               )
             )
-          );
-        })
+          ),
+          React.createElement('div', { className: 'square', id: 'work' }),
+          React.createElement('div', { className: 'square', id: 'shop' })
+        )
       );
     }
   }]);
--- src/home.js
+++ src/home.js
@@ -2,11 +2,24 @@
 
 const e = React.createElement;
 
-
-
+var entered = false;
 var x, y;
 var tailLength = 45;
 
+// enter site
+
+//TODO: swap two lines below
+document.onclick = () => {
+//window.onload = () => {
+  entered = true;
+  let presses = document.getElementsByClassName('press');
+  for (let i = 0; i < presses.length; i++) {
+    if(presses[i].id !== 'nodelete') {
+      presses[i].style.display = 'none';
+    }
+  }
+  document.getElementsByClassName('over')[0].className = 'over entered';
+}
 
 class Home extends React.Component {
   constructor(props) {
@@ -14,19 +27,29 @@ class Home extends React.Component {
     this.onclick = props.onclick;
   }
 
+
   componentDidMount() {
+    // let isDesktop = () => {
+    //   console.log(document.getElementById('homeMobile').style);
+    //   document.getElementById('homeDesktop').style.display !== 'none';
+    // }
+
+
     let element = this.refs.elem;
     let animationIsActive = false;
     let framesPerSecond = 60;
     let intervalDelay = Math.round(1000 / framesPerSecond);
     //let lagTailLength = 5;
 
-
     let i = 0;
     let z = 0;
 
-    let startAnimation = () => {
 
+    // if(!isDesktop()) {
+    //   return;
+    // }
+
+    let startAnimation = () => {
       return window.setInterval(() => {
         console.log('go');
         i = ((i+1)%tailLength);
@@ -41,7 +64,7 @@ class Home extends React.Component {
     let interval = null; // id of interval callback to animate
     window.addEventListener('mousemove', function(event){
         window.clearTimeout(timeout);
-        if(!animationIsActive) {
+        if(!animationIsActive && !entered) {
           interval = startAnimation();
           animationIsActive = true;
         }
@@ -61,22 +84,74 @@ class Home extends React.Component {
 
   render() {
     return (
-      <div ref='cont'>
-        <div className='home center'>
-          <div className='welcome'>
-            <svg viewBox="0 0 417 60">
-              <text y="57">WELCOME</text>
-            </svg>
+      <div className='home'>
+        <div className='homeDesktop' id='homeDesktop'>
+          <div className='home center'>
+            <div className='welcome'>
+              <svg viewBox="0 0 417 60">
+                <text y="57">WELCOME</text>
+              </svg>
+            </div>
+          </div>
+          {
+            Array.apply(null, Array(tailLength)).map((i, j) => <div id={'n'+j} key={j} className='press' ref={j === 0 ? 'elem' : ''}>
+              <svg viewBox="0 0 417 60">
+                <text y="57">PRESS</text>
+              </svg>
+            </div>)
+          }
+          <div className='over'>
+            <div className='left'>
+              <div className='welcome rot'>
+                <svg viewBox="0 0 417 60">
+                  <text y="57">WELCOME</text>
+                </svg>
+              </div>
+              <div className='press rot' id='nodelete'>
+                <svg viewBox="0 0 417 60">
+                  <text y="57">PRESS</text>
+                </svg>
+              </div>
+            </div>
+            <div className='right'>
+              <div className='top'>
+                <svg viewBox="0 0 417 60">
+                  <text x="50%" y="50%" alignmentBaseline="middle" textAnchor="middle">
+                    WORK
+                  </text>
+                </svg>
+              </div>
+              <div className='bottom'>
+                <svg viewBox="0 0 417 60">
+                  <text x="50%" y="50%" alignmentBaseline="middle" textAnchor="middle">
+                    SHOP
+                  </text>
+                </svg>
+              </div>
+            </div>
           </div>
         </div>
-        {
-          Array.apply(null, Array(tailLength)).map((i, j) => <div id={'n'+j} key={j} className='press' ref={j === 0 ? 'elem' : ''}>
-            <svg viewBox="0 0 417 60">
-              <text y="57">PRESS</text>
-            </svg>
-          </div>)
-
-        }
+        <div className='homeMobile' id='homeMobile' ref='homeMobile'>
+          <div className='homeBox'>
+            <div className='welcome'>
+              <svg viewBox="0 0 417 60">
+                <text y="57">WELCOME</text>
+              </svg>
+            </div>
+            <div className='press'>
+              <svg viewBox="0 0 417 60">
+                <text y="57">PRESS</text>
+              </svg>
+            </div>
+            <div className='questionBox'>
+              <span>?</span>
+            </div>
+          </div>
+          <div className='square' id='work'>
+          </div>
+          <div className='square' id='shop'>
+          </div>
+        </div>
       </div>
     );
   }
