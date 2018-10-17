bd55452f6fb1dbdac1ed31d8f8b212041d4c476e
Travis Newgren <travisnewgren@gmail.com>
2018-10-17 15:17:33 -0500

    staging mobile testing

--- scripts/mobilePush.sh
+++ scripts/mobilePush.sh
@@ -2,6 +2,6 @@ git add -A
 git commit -m "staging mobile testing"
 git push origin dev
 cd ../accel/
-git pull upstream dev
+git pull --no-edit upstream dev
 git push origin master
 cd ../welcomepress/
